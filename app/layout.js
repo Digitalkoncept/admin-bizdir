c3aba1d2f355ae4b0aaf9cf5279536e1a6b4516a
Saurabh <imsauby@gmail.com>
2024-05-03 16:52:40 +0530

    small changes and fixes

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,33 @@
 /* eslint-disable @next/next/no-head-element */
-'use client'
-import React,{useState} from "react";
+"use client";
+import React, { useState } from "react";
 import TopNav from "@/components/Admin/TopNav";
-import LeftSidebar from "@/components/Admin/LeftSidebar/LeftSidebar"
+import LeftSidebar from "@/components/Admin/LeftSidebar/LeftSidebar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import "@/styles/admin-style.css"
-
+import "@/styles/admin-style.css";
+import "@/css/bootstrap.css"
 
 export default function RootLayout({ children, session }) {
-  const [showadMenu,setShowAdMenu] = useState(true);
+  const [showadMenu, setShowAdMenu] = useState(true);
   function toggleCart() {
     setShowAdMenu(!showadMenu);
   }
   return (
-        <html>
-          <body>
-            <div>
-            <TopNav toggleCart={toggleCart} showadMenu={showadMenu}/>
-        <LeftSidebar showadMenu={showadMenu} />
-        {children}
-              <ToastContainer />
-            </div>
-          </body>
-        </html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div>
+          <TopNav toggleCart={toggleCart} showadMenu={showadMenu} />
+          <LeftSidebar showadMenu={showadMenu} />
+          {children}
+          <ToastContainer />
+        </div>
+      </body>
+    </html>
   );
 }
