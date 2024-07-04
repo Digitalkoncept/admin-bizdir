33e10711f510f049140d7c7df02f91dcdd1acc00
Digitalkoncept <110587750+Digitalkoncept@users.noreply.github.com>
2024-07-04 10:06:30 +0000

    created graphql login

--- middleware.js
+++ middleware.js
@@ -1,14 +1,11 @@
-
-
 import { withAuth } from "next-auth/middleware";
 import { verifyAuth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  async function middleware(req,res) {
-    
-    const Token = req.nextauth?.token.jwt;
-    // console.log("middlware running",Token)
+  async function middleware(req) {
+    const token = req.nextauth?.token?.jwt;
+
     const publicFile = /\.(.*)$/.test(req.nextUrl.pathname);
     if (req.nextUrl.pathname.startsWith("/login") || publicFile) {
       return NextResponse.next();
@@ -18,44 +15,54 @@ export default withAuth(
     if (req.nextUrl.pathname.startsWith("/_next") || req.nextUrl.pathname.startsWith("/api")) {
       return NextResponse.next();
     }
-      if (Token === undefined) {
-        console.log("token undefined")
-        return NextResponse.redirect(new URL('/login'));
+
+    if (!token) {
+      console.log("Token undefined");
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    try {
+      // Verify the token using your verifyAuth function
+      const verifiedToken = await verifyAuth(token);
+
+      const routePermission = {
+        '/all-employee': 'view employee',
+        '/create-employee': 'Create Employee',
+        '/all-roles': 'Roles',
+        '/create-roles': 'Create Roles',
+        '/all-users': 'Users',
+        '/admin-all-listings': 'view listings',
+        '/add-new-listing': 'Create Listing',
+        '/new-listing-request': 'New Listing Request',
+        '/all-category': 'Listing Category',
+        '/admin-add-new-category': 'Add Listing Category',
+        '/admin-all-sub-category': 'Listing Sub Category',
+        '/admin-add-new-sub-category': 'Add Listing Sub Category',
+        '/admin/ecommerce/shipments': 'Shipments',
+        '/admin/ecommerce/categories': 'Categories'
+      };
+
+      const requiredPermission = routePermission[req.nextUrl.pathname];
+      const hasPermission = verifiedToken.permissions.includes(requiredPermission);
+
+      if (requiredPermission && !hasPermission) {
+        return NextResponse.redirect(new URL('/', req.url));
       }
-      try {
-        // Verify the token using jwt.verify method
-        const verifiedToken =  await verifyAuth(Token);// Replace "your-secret-key" with your actual secret key
-        // console.log("token verification ",verifiedToken)
-        const routePermission = {
-          '/all-employee':'view employee',
-          '/create-employee':'Create Employee',
-          '/all-roles':'Roles',
-          '/create-roles':'Create Roles',
-          '/all-users':'Users',
-          '/admin-all-listings':'All Listings',
-          '/add-new-listing':'Create Listing',
-          '/new-listing-request':'New Listing Request',
-          '/all-category':'Listing Category',
-          '/admin-add-new-category':'Add Listing Category',
-          '/admin-all-sub-category':'Listing Sub Category',
-          '/admin-add-new-sub-category':'Add Listing Sub Category', 
-          '/admin/ecommerce/shipments':'Shipments',
-          '/admin/ecommerce/shipments':'Categories'
-        }
-        const requiredPermission = routePermission[req.nextUrl.pathname];
-        const hasPermission = req.nextauth.token.permissions.includes(requiredPermission);
-        if(requiredPermission && !hasPermission){
-           return NextResponse.redirect(new URL('/',req.url));
-        }
-      } catch (error) {
-        console.error("An error occurred:", error);
-        console.log(error)
-        // Redirect the user to a specific page when the token is not verified
-        return NextResponse.redirect(new URL('/login') );
+
+      return NextResponse.next();
+    } catch (error) {
+      console.error("An error occurred:", error);
+      // Redirect the user to the login page when the token is not verified
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized({ token }) {
+        return !!token;
       }
-    },
-  
+    }
+  }
 );
 
-
-export const config = { matcher: [ '/((?!login|api|_next/static|_next/image|favicon.ico|/public/:path))'] }
+export const config = { matcher: ['/((?!login|api|_next/static|_next/image|favicon.ico|public/:path).*)'] };
