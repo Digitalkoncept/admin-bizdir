2ad683dc3d50dfeabf25f0fa1261c2be6ff93d2a
Saurabh <saurabh@techcorona.com>
2024-07-08 13:14:15 +0530

    added jobs to sidebar

--- middleware.js
+++ middleware.js
@@ -7,19 +7,21 @@ export default withAuth(
     const token = req.nextauth?.token?.jwt;
 
     const publicFile = /\.(.*)$/.test(req.nextUrl.pathname);
-    if (req.nextUrl.pathname.startsWith("/login") || publicFile ) {
+    if (req.nextUrl.pathname.startsWith("/login") || publicFile) {
       return NextResponse.next();
     }
-    
 
     // Exclude static files from being intercepted by middleware
-    if (req.nextUrl.pathname.startsWith("/_next") || req.nextUrl.pathname.startsWith("/api")) {
+    if (
+      req.nextUrl.pathname.startsWith("/_next") ||
+      req.nextUrl.pathname.startsWith("/api")
+    ) {
       return NextResponse.next();
     }
 
     if (token === undefined) {
       console.log("Token undefined");
-      return NextResponse.redirect(new URL('/login', req.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
 
     try {
@@ -27,53 +29,56 @@ export default withAuth(
       const verifiedToken = await verifyAuth(token);
 
       const routePermission = {
-        '/all-employee': 'view employee',
-        '/create-employee': 'Create Employee',
-        '/all-jobs':'view jobs',
-        '/add-job':'add job',
-        '/assigned-jobs':'view  assigned jobs',
-        '/assign-new-job':'assign new job',
-        '/all-roles': 'view roles',
-        '/add-role': 'add role',
-        '/all-users': 'view Users',
-        '/admin-all-listings': 'view listings',
-        '/add-new-listing': 'add listing',
-        '/new-listing-request': 'view listing request',
-        '/all-listing-category': 'view listing category',
-        '/new-claim-request': 'view claim request',
-        '/add-listing-category': 'add listing category',
-        '/all-listing-sub-category': 'view listing sub category',
-        '/add-listing-sub-category': 'add listing sub category',
-        '/view-company-enquiry':'view company enquiry',
-        '/view-client-enquiry':'view client enquiry',
-        '/all-coupons':'view coupons',
-        '/add-new-coupon':'add coupon',
-        '/all-notifications':'view notifications'
-
-
+        "/all-employee": "view employee",
+        "/create-employee": "add employee",
+        "/all-jobs": "view jobs",
+        "/add-job": "add job",
+        "/assigned-jobs": "view assigned jobs",
+        "/assign-new-job": "assign new job",
+        "/all-roles": "view roles",
+        "/add-role": "add role",
+        "/all-users": "view users",
+        "/admin-all-listings": "view listings",
+        "/add-new-listing": "add listing",
+        "/new-listing-request": "view listing request",
+        "/all-listing-category": "view listing category",
+        "/new-claim-request": "view claim request",
+        "/add-listing-category": "add listing category",
+        "/all-listing-sub-category": "view listing sub category",
+        "/add-listing-sub-category": "add listing sub category",
+        "/view-company-enquiry": "view company enquiry",
+        "/view-client-enquiry": "view client enquiry",
+        "/all-coupons": "view coupons",
+        "/add-new-coupon": "add coupon",
+        "/all-notifications": "view notifications",
       };
 
       const requiredPermission = routePermission[req.nextUrl.pathname];
-      const hasPermission = verifiedToken.permissions.includes(requiredPermission);
+      const hasPermission =
+        verifiedToken.permissions.includes(requiredPermission);
 
       if (requiredPermission && !hasPermission) {
-        return NextResponse.redirect(new URL('/', req.url));
+        return NextResponse.redirect(new URL("/", req.url));
       }
-      console.log("middleware is running")
+      console.log("middleware is running");
       return NextResponse.next();
     } catch (error) {
       console.error("An error occurred:", error);
       // Redirect the user to the login page when the token is not verified
-      return NextResponse.redirect(new URL('/login', req.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
   },
   {
     callbacks: {
       async authorized({ token }) {
         return !!token;
-      }
-    }
+      },
+    },
   }
 );
 
-export const config = { matcher: [ '/((?!login|api|_next/static|_next/image|favicon.ico|/public/:path*)):path*'] }
+export const config = {
+  matcher: [
+    "/((?!login|api|_next/static|_next/image|favicon.ico|/public/:path*)):path*",
+  ],
+};
